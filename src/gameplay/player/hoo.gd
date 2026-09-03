e55129e1c61dead83b72288946aaed7c87624073
Juan Pastrana <juanpablotwo@gmail.com>
2026-09-03 12:09:03 -0400

    Simple movement

---
+++ src/gameplay/player/hoo.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+@export var speed : float = 500
+var direction : Vector2
+
+func _physics_process(delta) -> void:
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, speed)
+	move_and_slide()
